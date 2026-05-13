e94d942fa24523a51c7d82d6bad4d6f66e607364
JesussGonzalez <140839110+JesussGonzalez@users.noreply.github.com>
2026-05-13 15:54:11 +0000

    links

--- vite-project/src/lib/supabase.js
+++ vite-project/src/lib/supabase.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kksqvgawqpsrheikuewx.supabase.co'
-const supabaseAnonKey = 'sb_publishable_-U9oZnR7rYVaxF-sXe-iGg_h2LP4xIR'
+// Usamos import.meta.env para que Vite detecte las variables de Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+// Validación simple para que sepas si las claves están llegando bien
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Error: No se encontraron las variables de entorno de Supabase.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
